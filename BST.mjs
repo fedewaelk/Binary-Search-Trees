b06646bf6dbde631ffcb8759a6ea1bf7b49577d8
FedeWaelk <fedewaelk@gmail.com>
2025-04-23 14:08:42 -0300

    Build a Tree class

--- BST.mjs
+++ BST.mjs
@@ -1,7 +1,17 @@
+// 1 - Build a Node class
 class Node {
   constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
   }
+}
+
+// 2 - Build a Tree class
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  // buildTree(array) {}
 }
